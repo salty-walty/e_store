beeaa44378196ccfe3ae2c28f85bd9ab4dfaeea3
salty-walty <waltonsj3@gmail.com>
2018-12-12 11:28:13 -0500

    added cart

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
+
 Rails.application.routes.draw do
-  devise_for :users
   root 'storefront#all_items'
-
-
+  
+  post 'add_to_cart' => 'cart#add_to_cart'
+  
+  get 'view_order' => 'cart#view_order'
+  get 'checkout' => 'cart#checkout'
   get 'categorical' => 'storefront#items_by_category'
   get 'branding' => 'storefront#items_by_brand'
   
+  devise_for :users
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
